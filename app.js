dc36c12fac5c06586dc4f8b3115df70fbaf5fdd8
tberkovac <tberkovac1@etf.unsa.ba>
2022-07-16 21:50:33 +0200

    Dodani CRUD endpointovi

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 
 
+const Joi = require('joi')
 const express = require('express')
 const app = express()
+app.use(express.json())
+
+const courses = [
+    { id: 1, name: 'Course 1'},
+    { id: 2, name: 'Course 2'},
+    { id: 3, name: 'Course 3'}
+]
 
 app.get('/', (req, res) => {
     res.send('Hello world')
@@ -9,15 +17,56 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
     //listu kurseva iz baze
-    res.send([1, 2, 3])
+    res.send(courses)
 })
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params) 
+    const course = courses.find( c => c.id === parseInt(req.params.id))
+    if(!course) return res.status(404).send('Course with given id not found')
+    res.send(course)
 })
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.query) 
+app.post('/api/courses', (req, res) => {
+    const {error} = validateCourse(req.body)    
+    if(error)
+        return res.status(400).send(error.details[0].message)
+
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
+
+app.put('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id))
+    if(!course) 
+        return res.status(404).send('Course with given id not found')
+
+    const {error} = validateCourse(req.body)    
+    if(error) 
+        return res.status(400).send(error.details[0].message)
+
+
+    course.name = req.body.name
+    res.send(course)
+})
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+    return schema.validate(course)
+}
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find( c => c.id === parseInt(req.params.id))
+    if(!course) return res.status(404).send('Course with given id not found')
+
+    const index = courses.indexOf(course)
+    courses.splice(index,1)
+    res.send(course)
 })
 
 const port = process.env.PORT || 3000
